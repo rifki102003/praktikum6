9684cd4a080155ca46f93f5271ad4deee2ef7e24
rezza fauzi <rezzaf205@gmail.com>
2021-12-11 21:28:26 +0700

    Menambahkan file praktikum6.py

---
+++ praktikum06ku.py
@@ -0,0 +1,142 @@
+from prettytable import PrettyTable
+
+print("Nama : Muhamad Rifki")
+print("NIM : 312110205")
+print("Kelas : TI.21.A.1")
+print("=======================")
+print()
+
+daftar = []
+x = PrettyTable()
+
+while True:
+    print("[ (L)Menampilkan , (T)Tambah, (U)Ubah, (H)Hapus, (C)Cari, (K)eluar ]")
+    tanya = input("Masukkan Pilihan : ")
+    if tanya == "L":
+        print("==== Daftar Nilai Mahasiswa ====")
+        no = 0
+        no += 1
+        x.field_names = ["No", "NIM", " NAMA", "TUGAS", "UTS", "UAS", "AKHIR"]
+        if not daftar:
+            x.field_names = ["No", "NIM", " NAMA", "TUGAS", "UTS", "UAS", "AKHIR"]
+            print(" D A T A  K O S O N G")
+        else:
+            for data in daftar:
+                x.add_row([no, data["nim"], data["nama"], data["tugas"], data["uts"], data["uas"], data["akhir"]])
+            print(x)
+    elif tanya == "T":
+        print("Tambah Data ")
+        nim_v = input("NIM : ")
+        nama_v = input("Nama Lengkap : ")
+        uts_v = input("Nilai UTS : ")
+        uas_v = input("Nilai UAS : ")
+        tugas_v = input("Nilai Tugas : ")
+        akhir_v = 0.3 * float(tugas_v) + 0.35 * float(uts_v) + 0.35 * float(uas_v)
+        daftar.append({"nim": nim_v, "nama": nama_v, "tugas": tugas_v, "uts": uts_v, "uas": uas_v, "akhir": akhir_v})
+        print()
+        print("==== Daftar Nilai ====")
+        i = 0
+        for data in daftar:
+            i += 1
+            x.field_names = ["No", "NIM", " NAMA", "TUGAS", "UTS", "UAS", "AKHIR"]
+            x.add_row([i, data["nim"], data["nama"], data["tugas"], data["uts"], data["uas"], data["akhir"]])
+        print(x)
+    elif tanya == "U":
+        print("EDIT FILE")
+        print("Data siapa yang akan diubah ?")
+        siapa = input("Masukkan NIM Mahasiswa yang akan diubah : ")
+
+        print("Data apa yang akan diubah ? : ")
+        mhs = input(" 1. Nama \n 2. Nilai Tugas \n 3. Nilai UTS \n 4. Nilai UAS\n Pilih dengan angka (1/2/3/4) : ")
+        if mhs == "1":
+            ubahnama = input("Silahkan masukan nama yang benar : ")
+            i = 0
+            d = next(item for item in daftar if item['nim'] == siapa)
+            d['nama'] = ubahnama
+            x.field_names = ["No", "NIM", " NAMA", "TUGAS", "UTS", "UAS", "AKHIR"]
+            i += 1
+            x.add_row([i, d["nim"], d["nama"], d["tugas"], d["uts"], d["uas"], d["akhir"]])
+            print(x)
+        elif mhs == "2":
+            ubahtugas = input("Masukkan Nilai Tugas yang benar : ")
+            i = 0
+            d = next(item for item in daftar if item['nim'] == siapa)
+            d['tugas'] = ubahtugas
+            lihatuts = d['uts']
+            lihatuas = d['uas']
+            lihatakhir = 0.3 * float(ubahtugas) + 0.35 * float(lihatuts) + 0.35 * float(lihatuas)
+            d['akhir'] = lihatakhir
+            x.field_names = ["No", "NIM", " NAMA", "TUGAS", "UTS", "UAS", "AKHIR"]
+            for data in daftar:
+                i += 1
+                x.add_row([i, data["nim"], data["nama"], data["tugas"], data["uts"], data["uas"], data["akhir"]])
+            print(x)
+        elif mhs == "3":
+            ubahuts = input("Masukkan Nilai UTS yang benar : ")
+            i = 0
+            d = next(item for item in daftar if item['nim'] == siapa)
+            d['uts'] = ubahuts
+            lihattugas = d['tugas']
+            lihatuas = d['uas']
+            lihatakhir = 0.3 * float(lihattugas) + 0.35 * float(ubahuts) + 0.35 * float(lihatuas)
+            d['akhir'] = lihatakhir
+            x.field_names = ["No", "NIM", " NAMA", "TUGAS", "UTS", "UAS", "AKHIR"]
+            for data in daftar:
+                i += 1
+                x.add_row([i, data["nim"], data["nama"], data["tugas"], data["uts"], data["uas"], data["akhir"]])
+            print(x)
+        elif mhs == "4":
+            ubahuas = input("Masukkan Nilai UAS yang benar : ")
+            i = 0
+            d = next(item for item in daftar if item['nim'] == siapa)
+            d['uas'] = ubahuas
+            lihattugas = d['tugas']
+            lihatuts = d['uts']
+            lihatakhir = 0.3 * float(lihattugas) + 0.35 * float(lihatuts) + 0.35 * float(ubahuas)
+            d['akhir'] = lihatakhir
+            x.field_names = ["No", "NIM", " NAMA", "TUGAS", "UTS", "UAS", "AKHIR"]
+            for data in daftar:
+                i += 1
+                x.add_row([i, data["nim"], data["nama"], data["tugas"], data["uts"], data["uas"], data["akhir"]])
+            print(x)
+        else:
+            print("PILIHAN SALAH")
+
+    elif tanya == "C":
+        print(" ========== PENCARIAN DATA ==========")
+        print(" Pencarian berdasarkan NIM ")
+        carinim = input("Masukkan NIM yang akan dicari : ")
+        xdata = next(item for item in daftar if item['nim'] == carinim)
+        print("NIM : ", carinim)
+        print("Nama : ", xdata['nama'])
+        print("Nilai Tugas : ", xdata['tugas'])
+        print("Nilai UTS : ", xdata['uts'])
+        print("Nilai UAS : ", xdata['uas'])
+        print("Nilai Akhir : ", xdata['akhir'])
+    elif tanya == "H":
+        print("HAPUS DATA BERDASARKAN NIM")
+        datahapus = input("Masukkan NIM data yang akan dihapus : ")
+        xhapus = next(item for item in daftar if item['nim'] == datahapus)
+        del xhapus['nim']
+        del xhapus['nama']
+        del xhapus['tugas']
+        del xhapus['uts']
+        del xhapus['uas']
+        del xhapus['akhir']
+        print("Data Berhasil Dihapus")
+        no = 0
+        no += 1
+        x.field_names = ["No", "NIM", " NAMA", "TUGAS", "UTS", "UAS", "AKHIR"]
+        if not daftar:
+            x.field_names = ["No", "NIM", " NAMA", "TUGAS", "UTS", "UAS", "AKHIR"]
+            print(" D A T A  K O S O N G")
+        else:
+            for data in daftar:
+                x.add_row([no, data["nim"], data["nama"], data["tugas"], data["uts"], data["uas"], data["akhir"]])
+            print(x)
+
+    elif tanya == "K":
+        print("ANDA KELUAR DARI APLIKASI")
+        break
+    else:
+        print("ANDA MEMILIH PILIHAN YANG SALAH")
---
+++ praktikum6.py
@@ -0,0 +1,83 @@
+x = {}
+
+while True:
+    header="PROGRAM INPUT NILAI MAHASISWA"
+    header2=("MENU UTAMA")
+    print(header.center(97,"="))
+    print()
+    print(header2.center(97,"_"))
+    c = input("\n(M)Menampilkan, (T)ambah, (U)bah), (H)apus, (C)ari, (K)eluar: ")
+
+    if c.lower() == 't':
+        print("Tambah Data")
+        nama = input("Nama\t\t: ")
+        nim = int(input("NIM\t\t\t: "))
+        uts = int(input("Nilai UTS\t: "))
+        uas = int(input("Nilai UAS\t: "))
+        tugas = int(input("Nilai Tugas\t: "))
+        akhir = tugas*30/100 + uts*35/100 + uas*35/100
+        x[nama] = nim, uts, uas, tugas, akhir
+
+    elif c.lower() == 'u':
+        print("Ubah Data")
+        nama = input("\tMasukkan Nama\t\t: ")
+        if nama in x.keys():
+            nim     = int(input("\tNIM      \t\t\t: "))
+            uts     = int(input("\tNilai UTS\t\t\t: "))
+            uas     = int(input("\tNilai UAS\t\t\t: "))
+            tugas   = int(input("\tNilai Tugas\t\t\t: "))
+            akhir   = tugas*30/100 + uts*35/100 + uas*35/100
+            x[nama] = nim, uts, uas, tugas, akhir
+        else:
+            print("Nama {0} tidak ditemukan".format(nama))
+
+    elif c.lower() == 'h':
+        print("Hapus Data")
+        nama = input("Masukkan Nama  : ")
+        if nama in x.keys():
+            del x[nama]
+        else:
+            print("Nama {0} Tidak Ditemukan".format(nama))
+
+    elif c.lower() == 'c':
+        print("Cari Data[case-sensitive]")
+        nama = input("Masukkan Nama : ")
+        if nama in x.keys():
+            print("="*73)
+            print("|                             Daftar Mahasiswa                          |")
+            print("="*73)
+            print("| Nama            |       NIM       |  UTS  |  UAS  |  Tugas  |  Akhir  |")
+            print("="*73)
+            print("| {0:15s} | {1:15d} | {2:5d} | {3:5d} | {4:7d} | {5:7.2f} |"
+                  .format(nama, nim, uts, uas, tugas, akhir))
+            print("="*73)
+        else:
+            print("Nama {0} Tidak Ditemukan".format(nama))
+
+    elif c.lower() == 'm':
+        if x.items():
+            print("="*78)
+            print("|                               Daftar Mahasiswa                             |")
+            print("="*78)
+            print("|No. | Nama            |       NIM       |  UTS  |  UAS  |  Tugas  |  Akhir  |")
+            print("="*78)
+            i = 0
+            for z in x.items():
+                i += 1
+                print("| {no:2d} | {0:15s} | {1:15d} | {2:5d} | {3:5d} | {4:7d} | {5:7.2f} |"
+                      .format(z[0][:13], z[1][0], z[1][1], z[1][2], z[1][3], z[1][4], no=i))
+            print("=" * 78)
+        else:
+            print("="*78)
+            print("|                               Daftar Mahasiswa                             |")
+            print("="*78)
+            print("|No. | Nama            |       NIM       |  UTS  |  UAS  |  Tugas  |  Akhir  |")
+            print("="*78)
+            print("|                                TIDAK ADA DATA                              |")
+            print("="*78)
+
+    elif c. lower() == 'k':
+        break
+
+    else:
+        print("Pilih menu yang tersedia")
